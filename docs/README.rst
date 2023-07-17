2d89c5aa1722838e2e00bd13c6100651529433d6
Antonio Sapuppo <asapuppo@mect.it>
2023-07-17 10:16:57 +0200

    add text on progress

--- docs/README.rst
+++
@@ -1,8 +0,0 @@
-# Namino_Library
-
-Namino, getting started
-
-The Namino boards can be programmed in Windows / Linux systems, using the Arduino IDE and VS code (PlatformIO) IDE environments. These have their own installation guides, already established. The aim of the guide is how to integrate existing tutorials with Namino features.
-Presentation
-
-Our Namino system has an ESP32-S3 integrated to front end/ terminal block capable of connecting, to a machine, using 24V I/O industrial signals, 0-10V, 0-20mA, thermocouples, RTD, PTC, NTC, load cell. The front end is managed by an NXP microcontroller, with its own logic, connected to the SPI interface. Furthermore, a connector for the 3.3V Arduino shields is available.
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,26 @@
-# -*- coding: utf-8 -*-
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Namino boards library"
+copyright = "2023, Namino Team"
+author = "Namino Team"
+
 
 needs_sphinx = '1.3'
 extensions = []
